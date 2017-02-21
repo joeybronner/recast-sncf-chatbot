d9dc1bc58e3c8a647da47e5211c1de00d0851b25
Joey Bronner <joeybronner@gmail.com>
2017-02-21 10:31:12 +0100

    Default response from bot in case of action null

--- public/controllers/bot.client.controller.js
+++ public/controllers/bot.client.controller.js
@@ -37,76 +37,82 @@ angular.module('mean').controller('BotController', ['$scope', '$http',
                     // Write Recast response in console
                     console.log(response.data);
 
-                    var sIntention = response.data.action.slug;
-                    var sReply = response.data.action.reply;
-                    var bStepDone = response.data.action.done;
+                    if (response.data.action === null) {
+                        var sDefaultReply = response.replies[0];
+                        $scope.writeBot(sDefaultReply);
+                    } else {
+                        var sIntention = response.data.action.slug;
+                        var sReply = response.data.action.reply;
+                        var bStepDone = response.data.action.done;
 
-                    // Stay up to date the conversation token (important!)
-                    $scope.conversationToken = response.data.conversation_token;
+                        // Stay up to date the conversation token (important!)
+                        $scope.conversationToken = response.data.conversation_token;
 
-                    // Greetings
-                    if (!$scope.stepGreetingsValidated && sIntention === 'bonjour') {
-                        $scope.writeBot(sReply);
-                        if (bStepDone) {
+                        // Greetings
+                        if (!$scope.stepGreetingsValidated && sIntention === 'bonjour') {
+                            $scope.writeBot(sReply);
+                            if (bStepDone) {
+                                $scope.stepGreetingsValidated = true;
+                            }
+                        } else if (!$scope.stepGreetingsValidated) {
+                            $scope.writeBot('Bonjour!');
                             $scope.stepGreetingsValidated = true;
                         }
-                    } else if (!$scope.stepGreetingsValidated) {
-                        $scope.writeBot('Bonjour!');
-                        $scope.stepGreetingsValidated = true;
-                    }
 
-                    // Destination
-                    if ($scope.destination === '' && sIntention === 'destination') {
-                        $scope.writeBot(sReply);
-                        if (bStepDone) {
-                            $scope.destination = response.data.memory.traindestination.raw;
-                        }
-                    }
-
-                    // Departure
-                    if ($scope.destination !== '' && $scope.departure === '' && sIntention === 'departure') {
-                        if (bStepDone) {
-                            $scope.departure = response.data.memory.traindeparture.raw;
-                        }
-                    }
-
-                    // SNCF Search/Result Departure/Destination next trains
-                    if ($scope.departure !== '' && $scope.destination !== '') {
-                        var iIdDeparture = getStationByName($scope.departure);
-                        var iIdDestination = getStationByName($scope.destination);
-                        $http({
-                            method: 'GET',
-                            url: '/sncf/nextdeparture/' + iIdDeparture + '/to/' + iIdDestination
-                        }).then(function successCallback (response) {
-
-                            console.log(response.data);
-
-                            var sDeparture = response.data.passages.$.gare;
-                            var aTrains = response.data.passages.train;
-
-                            if (aTrains === undefined || aTrains.length === 0) {
-                                $scope.writeBot('Je n\'ai trouvé aucun train pour ce trajet, désolé.');
-                                $scope.writeBot('Au revoir.');
-                                _init();
-                            } else {
-                                var sDestination = aTrains[0].term[0];
-                                var dFirstDeparture = aTrains[0].date[0]._;
-                                var dFirstDepartureNum = aTrains[0].num[0];
-                                var dSecondDeparture = aTrains[1].date[0]._;
-                                var dSecondDepartureNum = aTrains[1].num[0];
-                                var dThirdDeparture = aTrains[2].date[0]._;
-                                var dThirdDepartureNum = aTrains[2].num[0];
-
-                                $scope.writeBot('Les trois prochains trains au départ de ' + getStationById(sDeparture) + ', et à destination de ' + getStationById(sDestination) + ' sont :');
-                                $scope.writeBot('➝ Train n° ' + dFirstDepartureNum + ' - ' + dFirstDeparture);
-                                $scope.writeBot('➝ Train n° ' + dSecondDepartureNum + ' - ' + dSecondDeparture);
-                                $scope.writeBot('➝ Train n° ' + dThirdDepartureNum + ' - ' + dThirdDeparture);
+                        // Destination
+                        if ($scope.destination === '' && sIntention === 'destination') {
+                            $scope.writeBot(sReply);
+                            if (bStepDone) {
+                                $scope.destination = response.data.memory.traindestination.raw;
                             }
+                        }
 
-                        }, function errorCallback (response) {
-                            $scope.writeBot(response);
-                        });
+                        // Departure
+                        if ($scope.destination !== '' && $scope.departure === '' && sIntention === 'departure') {
+                            if (bStepDone) {
+                                $scope.departure = response.data.memory.traindeparture.raw;
+                            }
+                        }
+
+                        // SNCF Search/Result Departure/Destination next trains
+                        if ($scope.departure !== '' && $scope.destination !== '') {
+                            var iIdDeparture = getStationByName($scope.departure);
+                            var iIdDestination = getStationByName($scope.destination);
+                            $http({
+                                method: 'GET',
+                                url: '/sncf/nextdeparture/' + iIdDeparture + '/to/' + iIdDestination
+                            }).then(function successCallback (response) {
+
+                                console.log(response.data);
+
+                                var sDeparture = response.data.passages.$.gare;
+                                var aTrains = response.data.passages.train;
+
+                                if (aTrains === undefined || aTrains.length === 0) {
+                                    $scope.writeBot('Je n\'ai trouvé aucun train pour ce trajet, désolé.');
+                                    $scope.writeBot('Au revoir.');
+                                    _init();
+                                } else {
+                                    var sDestination = aTrains[0].term[0];
+                                    var dFirstDeparture = aTrains[0].date[0]._;
+                                    var dFirstDepartureNum = aTrains[0].num[0];
+                                    var dSecondDeparture = aTrains[1].date[0]._;
+                                    var dSecondDepartureNum = aTrains[1].num[0];
+                                    var dThirdDeparture = aTrains[2].date[0]._;
+                                    var dThirdDepartureNum = aTrains[2].num[0];
+
+                                    $scope.writeBot('Les trois prochains trains au départ de ' + getStationById(sDeparture) + ', et à destination de ' + getStationById(sDestination) + ' sont :');
+                                    $scope.writeBot('➝ Train n° ' + dFirstDepartureNum + ' - ' + dFirstDeparture);
+                                    $scope.writeBot('➝ Train n° ' + dSecondDepartureNum + ' - ' + dSecondDeparture);
+                                    $scope.writeBot('➝ Train n° ' + dThirdDepartureNum + ' - ' + dThirdDeparture);
+                                }
+
+                            }, function errorCallback (response) {
+                                $scope.writeBot(response);
+                            });
+                        }
                     }
+
                 });
             }
         };
